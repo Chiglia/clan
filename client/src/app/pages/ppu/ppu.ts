e147d2a9fff7f289af922ae0bca9d752a4881170
Davide Chigliaro <davide.chigliaro@grupposcai.it>
2026-02-04 12:01:40 +0100

    Fix: ppu page corretta

--- client/src/app/pages/ppu/ppu.ts
+++ client/src/app/pages/ppu/ppu.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { afterNextRender, Component, NgZone, OnInit } from '@angular/core';
 import { SharedModule } from '../../../shared.module';
 import { Ragazzo } from '../../interfaces/ragazzo';
 import { RagazziService } from '../../services/ragazzi.service';
@@ -14,13 +14,19 @@ export class Ppu implements OnInit {
   ragazzi: Ragazzo[] = [];
   loading = true;
 
-  constructor(public ragazziService: RagazziService) {}
-
-  async ngOnInit() {
-    this.loading = true;
-    this.ragazzi = await this.ragazziService.getAllRagazzi();
-    await new Promise((r) => setTimeout(r, 200));
-    this.loading = false;
-    console.log(this.ragazzi);
+  constructor(
+    public ragazziService: RagazziService,
+    private ngZone: NgZone,
+  ) {
+    afterNextRender(async () => {
+      const data = await this.ragazziService.getAllRagazzi();
+      await new Promise((r) => setTimeout(r, 300));
+      this.ngZone.run(() => {
+        this.ragazzi = data;
+        this.loading = false;
+      });
+    });
   }
+
+  ngOnInit() {}
 }
